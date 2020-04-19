dc008c10c6e32c35d4b5dcd7a3fdff1a01c66faa
Eric Oliver <eoliver4@my.apsu.edu>
2020-04-18 19:41:25 -0500

    Fixing the table inserts and adding some more defaults

--- Magic_Tools/Table_Setup.sql
+++ Magic_Tools/Table_Setup.sql
@@ -1,15 +1,42 @@
 DROP TABLE IF EXISTS Card_Types;
 DROP TABLE IF EXISTS Card_Subtypes;
+DROP TABLE IF EXISTS Legalities;
+DROP TABLE IF EXISTS LegalTypes;
+DROP TABLE IF EXISTS DeckList;
+DROP TABLE IF EXISTS Decks;
 DROP TABLE IF EXISTS Colors;
 DROP TABLE IF EXISTS Types;
 DROP TABLE IF EXISTS Subtypes;
 DROP TABLE IF EXISTS Cards;
 DROP TABLE IF EXISTS Sets;
+DROP TABLE IF EXISTS Formats;
+DROP TABLE IF EXISTS ColorIdentity;
+DROP TABLE IF EXISTS Supertypes;
+
+CREATE TABLE Formats (
+	 FormatID INT AUTO_INCREMENT
+	,FormatName VARCHAR(20)
+	,PRIMARY KEY(FormatID)
+);
 
 CREATE TABLE Colors (
 	 ColorID INT AUTO_INCREMENT
 	,Color VARCHAR(10)
+	,ColorSymbol VARCHAR(10)
 	,PRIMARY KEY (ColorID)
+);
+
+CREATE TABLE ColorIdentity (
+	 CIdentityID INT AUTO_INCREMENT
+	,CIdentityName VARCHAR(50)
+	,CIdentitySymbol VARCHAR(10)
+	,PRIMARY KEY (CIdentityID)
+);
+
+CREATE TABLE Supertypes (
+	 SupertypeID INT AUTO_INCREMENT
+	,SupertypeDesc VARCHAR(20)
+	,PRIMARY KEY (SupertypeID)
 );
 
 CREATE TABLE Types (
@@ -25,23 +52,25 @@ CREATE TABLE Subtypes (
 );
 
 CREATE TABLE Sets (
-	 SetID INT AUTO_INCREMENT
-	,SetCode VARCHAR(10)
+	 SetCode VARCHAR(10)
 	,SetName VARCHAR(100)
-	,PRIMARY KEY (SetID)
+	,PRIMARY KEY (SetCode)
 );
 
 CREATE TABLE Cards (
 	 CardID INT AUTO_INCREMENT
 	,CMC INT
+	,ImageURL VARCHAR(4000)
 	,ManaCost VARCHAR(10)
 	,NAME VARCHAR(100)
+	,Number VARCHAR(10)
 	,Power VARCHAR(10)
 	,Rarity VARCHAR(20)
-	,SetID INT
+	,SetCode VARCHAR(10)
 	,Text VARCHAR(4000)
 	,Toughness VARCHAR(10)
 	,PRIMARY KEY (CardID)
+	,FOREIGN KEY (SetCode) REFERENCES Sets(SetCode)
 );
 
 CREATE TABLE Card_Types (
@@ -62,11 +91,113 @@ CREATE TABLE Card_Subtypes (
 	,FOREIGN KEY (SubtypeID) REFERENCES Subtypes(SubtypeID)
 );
 
+CREATE TABLE Decks (
+	 DeckID INT AUTO_INCREMENT
+	,DeckName VARCHAR(25)
+	,FormatID INT
+	,PRIMARY KEY (DeckID)
+	,FOREIGN KEY (FormatID) REFERENCES Formats(FormatID)
+);
+
+CREATE TABLE DeckList (
+	 DListID INT AUTO_INCREMENT
+	,CardID INT
+	,NumberCopies INT
+	,PRIMARY KEY (DListID)
+	,FOREIGN KEY (CardID) REFERENCES Cards(CardID)
+);
+
+CREATE TABLE LegalTypes (
+	 LTypeID INT AUTO_INCREMENT
+	,LTypeDesc VARCHAR(10)
+	,PRIMARY KEY (LTypeID)
+);
+
+CREATE TABLE Legalities (
+	 LegalID INT AUTO_INCREMENT
+	,FormatID INT
+	,CardID INT
+	,LTypeID INT
+	,PRIMARY KEY (LegalID)
+	,FOREIGN KEY (FormatID) REFERENCES Formats(FormatID)
+	,FOREIGN KEY (CardId) REFERENCES Cards(CardID)
+	,FOREIGN KEY (LTypeID) REFERENCES LegalTypes(LTypeID)
+);
+
 INSERT INTO Colors(
-	Color
+	 Color
+	,ColorSymbol
 ) VALUES
-	 ('Black')
-	,('Blue')
-	,('Green')
-	,('Red')
-	,('White');
+	 ('Black','B')
+	,('Blue','U')
+	,('Green','G')
+	,('Red','R')
+	,('White','W');
+
+INSERT INTO ColorIdentity(
+	CIdentityName
+	,CIdentitySymbol
+) VALUES
+	-- Mono Colors
+	 ('Mono Black','B')
+	,('Mono Blue','U')
+	,('Mono Green','G')
+	,('Mono Red','R')
+	,('Mono White','W')
+
+	-- Dual Colors
+	,('Aorious','WU')
+	,('Boros','RW')
+	,('Dimir','UB')
+	,('Golgari','BG')
+	,('Gruul','RG')
+	,('Izzet','UR')
+	,('Orzhov','WB')
+	,('Rakdos','BR')
+	,('Selesnya','GW')
+	,('Simic','GU')
+
+	-- Tri Colors
+	,('Abzan','BWG')
+	,('Bant','WGU')
+	,('Esper','UWB')
+	,('Grixis','BRU')
+	,('Jeskai','URW')
+	,('Jund','RGB')
+	,('Mardu','RWB')
+	,('Naya','GWR')
+	,('Sultai','BGU')
+	,('Temur','GRU')
+
+	-- Quad Colors
+	,('Non-White','UBRG')
+	,('Non-Blue','BRGW')
+	,('Non-Black','RGWB')
+	,('Non-Red','GWUB')
+	,('Non-Green','WUBR')
+
+	-- Other
+	,('Rainbow','WUBRG')
+	,('Diamond','D');
+
+INSERT INTO LegalTypes (
+	LTypeDesc
+) VALUES
+	 ('Legal')
+	,('Banned')
+	,('Restricted');
+
+INSERT INTO Formats (
+	 FormatName
+) VALUES
+	 ('Standard')
+	,('Commander')
+	,('Duel')
+	,('Legacy')
+	,('Modern')
+	,('Pioneer')
+	,('Vintage')
+	,('Pauper')
+	,('Brawl')
+	,('Future')
+	,('Historic');
