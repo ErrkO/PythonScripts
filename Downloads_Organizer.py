442067336ec516639ec09408af4e661ad9c08d2a
Eric Oliver <eoliver4@my.apsu.edu>
2020-08-19 13:28:55 -0500

    Saving work to downloads organizer

--- Downloads_Organizer.py
+++ Downloads_Organizer.py
@@ -10,7 +10,7 @@ Code = [ '.py','.pdb','.sql' ]
 Document = [ '.epub','.html','.pdf','.tex','.txt' ]
 Extension = [ '.vsix' ]
 Image = [ '.ico','.png','.jpg' ]
-Other = [ '.bin','.cache' ]
+Other = [ '.bin','.cache', '.ct' ]
 Videos = [ '.avi' ]
 
 Folders = [ Applications, Archives, Code,
@@ -28,10 +28,18 @@ def IsFile(file):
     else:
         return True
 
+def IsOfExtension(file,lst):
+    for ext in lst:
+        if file == ext:
+            return True
+    return False
+
 def SortFile(file):
     splits = re.findall(FileExtension_Regex,file)
     pass
 
+def GrabExtension(file):
+    return re.split(FileExtension_Regex,file[:-5])[1]
 
 def Main():
     for file in os.listdir(Downloads_Path):
